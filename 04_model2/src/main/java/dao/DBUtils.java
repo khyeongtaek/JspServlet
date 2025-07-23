ad89b2643c0a4e17b0e22f8c06f1476da1127593
kht <gudxor1541@naver.com>
2025-07-23 10:48:36 +0900

    04_model2 프로젝트 초기 세팅

---
+++ 04_model2/src/main/java/dao/DBUtils.java
@@ -0,0 +1,38 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class DBUtils {
+
+    // private 생성자 : 외부에서 new DBUtils() 호출 방지
+    private DBUtils() {
+
+    }
+
+    public static Connection getConnection() {
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(
+                    "jdbc:mysql://kwonht.synology.me:33307/db_jdbc?characterEncoding=UTF-8&serverTimezone=UTC",
+                    "user", "root");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
+
+    public static void close(Connection con, PreparedStatement ps, ResultSet rs) {
+        try {
+            if (rs != null) rs.close();
+            if (ps != null) ps.close();
+            if (con != null) con.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
